1cd17e46551643a537282dc656463f0342cbb3c2
Martha Kalisz <mskalisz3@gmail.com>
2021-03-02 23:53:02 +0000

    Update question 1 to include the prompt

--- 1:Basics/1.rb
+++ 1:Basics/1.rb
@@ -1,5 +1,11 @@
+=begin
+Add two strings together that, when concatenated, 
+return your first and last name as your full name in one string.
+=end
+
 first_name = "Martha"
 last_name = "Kalisz"
+
 #string concatination
 puts first_name + ' ' + last_name
 
