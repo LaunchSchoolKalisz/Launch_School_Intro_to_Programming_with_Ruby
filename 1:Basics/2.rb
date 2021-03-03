9fab3da856f8edcc6898d57a1f65c505c9a33df3
Martha Kalisz <mskalisz3@gmail.com>
2021-03-03 00:05:53 +0000

    Completed exercise 2

---
+++ 1:Basics/2.rb
@@ -0,0 +1,14 @@
+=begin
+Use the modulo operator, division, or a combination 
+of both to take a 4 digit number and find the digit in the: 
+1) thousands place 2) hundreds place 3) tens place 4) ones place
+=end
+thousands = 1234 / 1000
+hundreds = 1234 / 100 % 10
+tens = 1234 / 10 % 10
+ones = 1234 % 10
+
+puts thousands
+puts hundreds
+puts tens
+puts ones
