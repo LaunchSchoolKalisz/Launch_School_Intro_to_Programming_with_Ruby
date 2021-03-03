13e701dc9972daa31917972dfc665145d597508d
Martha Kalisz <mskalisz3@gmail.com>
2021-03-03 12:12:50 +0000

    Solve exercise 6

---
+++ 1:Basics/6.rb
@@ -0,0 +1,13 @@
+=begin 
+Write a program that calculates the squares
+of 3 float numbers of your choosing and outputs 
+the result to the screen.
+=end
+
+squares1 = 3.3 * 3.3
+squares2 = 393.393 * 393.393
+squares3 = 3393.3393 * 3393.3393
+
+puts squares1
+puts squares2
+puts squares3
