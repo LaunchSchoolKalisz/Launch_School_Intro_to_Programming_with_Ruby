a33d871b986bd79ae164f3512466f38c49837e1d
Martha Kalisz <mskalisz3@gmail.com>
2021-03-03 11:58:57 +0000

    Solved exercise 5

---
+++ 1:Basics/5.rb
@@ -0,0 +1,15 @@
+#Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+#This does not feel very efficient... but I don't know how to write a
+#loop in ruby yet
+num5 = 5
+fact5 = num5 * (num5 - 1)
+num6 = 6
+fact6 = num6 * (num6 - 1)
+num7 = 7
+fact7 = num7 * (num7 - 1)
+num8 = 8
+fact8 = num8 * (num8 - 1)
+puts fact5
+puts fact6
+puts fact7
+puts fact8
