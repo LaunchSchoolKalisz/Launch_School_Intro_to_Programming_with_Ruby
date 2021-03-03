76487be316d580660278d60441ff7b2a57535c9f
Martha Kalisz <mskalisz3@gmail.com>
2021-03-03 12:20:52 +0000

    Solve exercise 7

---
+++ 1:Basics/7.rb
@@ -0,0 +1,10 @@
+=begin
+What does the following error message tell you?
+SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
+  
+  The user was trying to create a hash but she closed the hash with
+  ")" rather than "}"... or maybe she opened the statement with the
+  incorrect character. Either way, something is mis-matched in this 
+  user's code
+=end
