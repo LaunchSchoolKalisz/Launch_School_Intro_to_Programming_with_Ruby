0173651f2b24778ab46d1233d68c5a7243293bc7
Martha Kalisz <mskalisz3@gmail.com>
2021-03-03 12:05:36 +0000

    Fix: corrected solution to exercise 5

--- 1:Basics/5.rb
+++ 1:Basics/5.rb
@@ -2,13 +2,13 @@
 #This does not feel very efficient... but I don't know how to write a
 #loop in ruby yet
 num5 = 5
-fact5 = num5 * (num5 - 1)
+fact5 = num5 * (num5 - 1) * (num5 - 2) * (num5 - 3) * (num5 - 4) 
 num6 = 6
-fact6 = num6 * (num6 - 1)
+fact6 = num6 * (num6 - 1) * (num6 - 2) * (num6 - 3) * (num6 - 4) * (num6 - 5)
 num7 = 7
-fact7 = num7 * (num7 - 1)
+fact7 = num7 * (num7 - 1) * (num7 - 2) * (num7 - 3) * (num7 - 4) * (num7 - 5) * (num7 - 6)
 num8 = 8
-fact8 = num8 * (num8 - 1)
+fact8 = num8 * (num8 - 1) * (num8 - 2) * (num8 - 3) * (num8 - 4) * (num8 - 5) * (num8 - 6) * (num8 -7)
 puts fact5
 puts fact6
 puts fact7
