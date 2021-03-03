71360ccf45b7a02c4b313dcf2cb816f5b706b62b
Martha Kalisz <mskalisz3@gmail.com>
2021-03-03 11:13:57 +0000

    Copy solution 3 into 4

---
+++ 1:Basics/4.rb
@@ -0,0 +1,34 @@
+=begin
+Write a program that uses a hash to store a list
+of movie titles with the year they came out. Then
+use the puts command to make your program print out the 
+year of each movie to the screen.
+=end
+movie_list = {  :"Terminator 2" => 1991, 
+                :"Mean Girls" => 2004, 
+                :"The Notebook" => 2004, 
+                :"Love Actually" => 2003, 
+                :"The Holiday" => 2006, 
+                :"Home Alone" =>1990  }
+
+puts movie_list[:"Terminator 2"] 
+puts movie_list[:"Mean Girls"]
+puts movie_list[:"The Notebook"]
+puts movie_list[:"Love Actually"]
+puts movie_list[:"The Holiday"]
+puts movie_list[:"Home Alone"]
+
+#New hash syntax (Ruby 1.9 and up)
+movies = {  "Terminator 2": 1991, 
+                "Mean Girls": 2004, 
+                "The Notebook": 2004, 
+                "Love Actually": 2003, 
+                "The Holiday": 2006, 
+                "Home Alone": 1990  }
+
+puts movies[:"Terminator 2"] 
+puts movies[:"Mean Girls"]
+puts movies[:"The Notebook"]
+puts movies[:"Love Actually"]
+puts movies[:"The Holiday"]
+puts movies[:"Home Alone"]
