b08935ffcdd536820d636abc668615f28868b50e
Martha Kalisz <mskalisz3@gmail.com>
2021-03-04 15:43:19 +0000

    Solve exercise 2

---
+++ 2:Variables/2age.rb
@@ -0,0 +1,16 @@
+=begin
+Write a program called age.rb that asks a user 
+how old they are and then tells them how old they will be 
+in 10, 20, 30 and 40 years. 
+=end
+
+puts "How old are you?"
+age = gets.chomp
+age_plus_ten = age.to_i + 10
+puts "In 10 years you will be:\n" + "#{age_plus_ten}"
+age_plus_twenty = age.to_i + 20
+puts "In 20 years you will be:\n" + "#{age_plus_twenty}"
+age_plus_thirty = age.to_i + 30
+puts "In 30 years you will be:\n" + "#{age_plus_thirty}"
+age_plus_fourty = age.to_i + 40
+puts "In 40 years you will be:\n" + "#{age_plus_fourty}"
