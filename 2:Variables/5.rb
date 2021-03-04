66a9d676dbaa89654d621a31929d94e978ec2c6a
Martha Kalisz <mskalisz3@gmail.com>
2021-03-04 18:54:30 +0000

    Docs: add comments to explain exercise 5

--- 2:Variables/5.rb
+++ 2:Variables/5.rb
@@ -1,5 +1,5 @@
 =begin
-The first program should run.
+The first program should run, outputting 3.
 
 The 2nd program would produce an error because the var
 x is defined in the inner code and called on the outer 
@@ -8,14 +8,14 @@ it exists
 =end
 
 #x = 0
-#3.times do
-#  x += 1
+#3.times do #iterate block x3
+#  x += 1   #shorthand for x = x+1, reassigning x
 #end
 #puts x
 
 y = 0
 3.times do
-  y += 1
-  x = y
+  y += 1   #reassign y with each iteration
+  x = y    #x is assigned to the same integer
 end
-puts x
+puts x     #initialized in the inner scope -->error
