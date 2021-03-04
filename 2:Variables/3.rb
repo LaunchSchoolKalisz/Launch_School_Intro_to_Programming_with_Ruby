0084ca810766720b5bd36e3d1525f085cd3020cd
Martha Kalisz <mskalisz3@gmail.com>
2021-03-04 16:11:11 +0000

    Solve exercise 3

---
+++ 2:Variables/3.rb
@@ -0,0 +1,11 @@
+=begin
+Add another section onto name.rb that prints
+the name of the user 10 times. You must do this without 
+explicitly writing the puts method 10 times in a row. 
+Hint: you can use the times method to do something repeatedly.
+=end
+
+puts 'Type in your name:'
+name = gets.chomp
+10.times {puts name}
+puts "Hi there, " + name + "!"
