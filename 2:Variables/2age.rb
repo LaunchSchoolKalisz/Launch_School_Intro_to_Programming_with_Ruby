6706ef1d7ea13696d5a02b0703190b41075189a3
Martha Kalisz <mskalisz3@gmail.com>
2021-03-04 15:56:18 +0000

    Update solution 2, more concise and meaningful

--- 2:Variables/2age.rb
+++ 2:Variables/2age.rb
@@ -4,25 +4,25 @@ how old they are and then tells them how old they will be
 in 10, 20, 30 and 40 years. 
 =end
 
-puts "How old are you?"
-age = gets.chomp
-age_plus_ten = age.to_i + 10
-puts "In 10 years you will be:\n" + "#{age_plus_ten}"
-age_plus_twenty = age.to_i + 20
+puts "How old are you?" #message to user
+age = gets.chomp.to_i #store input into local variable, and convert to integer
+age_plus_ten = age + 10 #another local variable to get age to output
+puts "In 10 years you will be:\n" + "#{age_plus_ten}" #output to user
+age_plus_twenty = age + 20
 puts "In 20 years you will be:\n" + "#{age_plus_twenty}"
-age_plus_thirty = age.to_i + 30
+age_plus_thirty = age + 30
 puts "In 30 years you will be:\n" + "#{age_plus_thirty}"
-age_plus_fourty = age.to_i + 40
+age_plus_fourty = age + 40
 puts "In 40 years you will be:\n" + "#{age_plus_fourty}"
 
 #LS solution (more concise):
-puts "How old are you?"
-age2 = gets.chomp.to_i
-puts "In 10 years you will be:"
-puts age + 10
-puts "In 20 years you will be:"
-puts age + 20
-puts "In 30 years you will be:"
-puts age + 30
-puts "In 40 years you will be:"
-puts age + 40
+puts "How old is your partner?"
+age_partner = gets.chomp.to_i
+puts "In 10 years your partner will be:"
+puts age_partner + 10
+puts "In 20 years your partner will be:"
+puts age_partner + 20
+puts "In 30 years your partner will be:"
+puts age_partner + 30
+puts "In 40 years your partner will be:"
+puts age_partner + 40
