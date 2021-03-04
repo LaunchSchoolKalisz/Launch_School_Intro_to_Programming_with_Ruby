2d9f8ff4de3fd93db01d3a0412c67d7450789be1
Martha Kalisz <mskalisz3@gmail.com>
2021-03-04 23:05:59 +0000

    Practice witth methods

--- 3:Methods/chaining.rb
+++ 3:Methods/chaining.rb
@@ -1,5 +1,27 @@
+=begin
 def add_three(n)
-  n + 3
+ new_value = n + 3
+ puts new_value
+ new_value
 end
 
-add_three(5)
+add_three(5).times { puts "this should print 8 times" }
+=end
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+#add(20, 45)
+#subtract(80, 10)
+
+def multiply (num1, num2)
+  num1 * num2
+end
+
+multiply(add(20,45), subtract(80,10))
+p add(subtract(80,10), multiply(subtract(20,6), add(30,5)))
