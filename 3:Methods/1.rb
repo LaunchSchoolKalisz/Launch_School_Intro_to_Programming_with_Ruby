49175ca4609f52b4c211c1f7a4808a974926f4eb
Martha Kalisz <mskalisz3@gmail.com>
2021-03-04 23:36:04 +0000

    Docs:add commentsto explain exercise 1

--- 3:Methods/1.rb
+++ 3:Methods/1.rb
@@ -5,7 +5,7 @@ that takes a name as its parameter and returns a string.
 =end
 
 def greeting(name)
-  "Welcome " + name + "!"
+  "Welcome " + name + "!" #if invoking puts here, return would be nil!
 end
 
 puts greeting("Martha")
