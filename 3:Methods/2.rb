f39dc6e2f98cff7d025f0b25650defc4e3d2bc8e
Martha Kalisz <mskalisz3@gmail.com>
2021-03-05 00:30:44 +0000

    Solve exercise 2 with comments from solution video

---
+++ 3:Methods/2.rb
@@ -0,0 +1,15 @@
+#What do the following expressions evaluate to?
+1. x = 2      #variable assignment, returns value assigned
+ => 2
+2. puts x = 2   #Variable assignment with puts method, outputs value but returns nil
+  2
+    => nil
+3. p name = "Joe" #variable assignment to a string, uses p, outputs its argument and returns its argument
+  "Joe"  
+    => "Joe"
+4. four = "four" #simple variable assignment, returns value assigned
+     => "four"
+5. print something = "nothing"  #Assign variable, pass to print method, outputs return then returns nil
+    nothing => nil            #print does not output a new line for its returns!
+
+
