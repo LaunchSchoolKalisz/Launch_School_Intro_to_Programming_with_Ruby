5beb968e718159c6b742aa083459bf0711284f75
Martha Kalisz <mskalisz3@gmail.com>
2021-03-05 02:03:49 +0000

    Add case_statementfile

--- 4:Flow_Control/conditional.rb
+++ 4:Flow_Control/conditional.rb
@@ -3,9 +3,36 @@ a = gets.chomp.to_i
 
 if a == 3
   puts "a is 3"
-  elsif a ==4
-    puts "a is 4"
-  else
-    puts "a is neither 3, nor 4"
-    
+elsif a ==4
+  puts "a is 4"
+else
+  puts "a is neither 3, nor 4"
 end
+
+#Practice writing ruby conditionals
+#1
+if x == 3
+  puts "x is 3"
+end
+#Same as:
+puts "x is 3" if x == 3
+
+#2
+if x == 3
+  puts "x is 3"
+elsif x == 4
+    puts "x is 4"
+end
+
+#3
+if x == 3
+  puts "x is 3"
+else
+  puts "x is NOT 3"
+end
+
+#4: Use 1 line syntax, must use 'then'
+if x == 3 then puts "x is 3" end
+
+#unless: opposite of if
+puts "x is NOT 3" unless x == 3
