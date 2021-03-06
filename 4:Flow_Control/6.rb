a1a40f2ed0599db74869929bd915aef7afeabe5a
Martha Kalisz <mskalisz3@gmail.com>
2021-03-06 16:26:19 +0000

    Fix solve exercise 6

---
+++ 4:Flow_Control/6.rb
@@ -0,0 +1,17 @@
+=begin
+Write down whether the following expressions return 
+true or false or raise an error. Then, type the expressions
+into irb to see the results.
+=end
+(32 * 4) >= "129"
+#Error: compare integer to string
+847 == '874'
+#False
+'847' < '846'
+#False
+'847' > '846'
+#True
+'847' > '8478'
+#False
+'847' < '8478'
+#True
