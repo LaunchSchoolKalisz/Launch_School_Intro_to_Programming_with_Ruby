19f976daf1430f1603b5c091689bb7808bee8848
Martha Kalisz <mskalisz3@gmail.com>
2021-03-06 13:50:41 +0000

    Add LS solution with comments

--- 4:Flow_Control/3.rb
+++ 4:Flow_Control/3.rb
@@ -18,5 +18,20 @@ else
   puts "The number you entered is outside of our scope, perhaps negative. Please try again"
 end
 
+#Solution by LS slightly different/more concise:
 
+puts "Please enter a number between 0 and 100" #Give user a message
+number = gets.chomp.to_i  #capture user input, store in variable, 
+                          #chomp removes new line,to_i gives us an integer so we can compare later
+
+if number < 0 
+  puts "You can't enter a negative number!"
+elsif number <= 50 #this statement is only evaluated if the first condition is passed!
+  puts "#{number} is between 0 and 50"
+elsif number <= 100 #this statement is only evaluated if the first 2 conditions are passed! (number is not neg and greater than 50)
+  puts "#{number} is between 51 and 100"
+else
+  puts "#{number} is above 100"
+end
+    
     
