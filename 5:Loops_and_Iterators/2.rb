c1e96983dfeccbf9a94e9149f8e3db5a5ec91108
Martha Kalisz <mskalisz3@gmail.com>
2021-03-06 20:45:10 +0000

    review exercise 2 with LS solution

--- 5:Loops_and_Iterators/2.rb
+++ 5:Loops_and_Iterators/2.rb
@@ -11,3 +11,13 @@ loop do
     break
   end
 end
+
+#Solution from LS
+
+x = ""
+while x != "STOP" do
+  puts "Hi, How are you feeling?"
+  ans = gets.chomp
+  puts "Want me to ask you again?"
+  x = gets.chomp
+end
