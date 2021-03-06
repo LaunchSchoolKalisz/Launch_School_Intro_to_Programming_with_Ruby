f9037af942c64b9b18c70c4c4f6d948e799ca43f
Martha Kalisz <mskalisz3@gmail.com>
2021-03-06 13:13:07 +0000

    Solve exercise 2

---
+++ 4:Flow_Control/2.rb
@@ -0,0 +1,16 @@
+=begin
+Write a method that takes a string as argument. 
+The method should return a new, all-caps version 
+of the string, only if the string is longer than 10 
+characters. Example: change "hello world" to "HELLO WORLD". 
+=end
+
+def all_caps(word)
+  if word.length >= 10
+   word.upcase
+  else
+    word
+  end
+end
+
+puts all_caps("hello world")
