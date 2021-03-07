853271d4d9ae62b6a701d4e9554335a122695af3
Martha Kalisz <mskalisz3@gmail.com>
2021-03-07 19:45:27 +0000

    Solve exercise 3

--- 5:Loops_and_Iterators/3.rb
+++ 5:Loops_and_Iterators/3.rb
@@ -1,1 +1,12 @@
 #Write a method that counts down to zero using recursion.
+
+def countdown (num)
+    puts num
+    if num > 0
+        countdown(num -= 1)
+    else 
+        puts "The countdown is done"
+    end
+end
+
+countdown(5)
