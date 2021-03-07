d73708aa7a976fbda11a9846bdb5a1fbf03cd1cc
Martha Kalisz <mskalisz3@gmail.com>
2021-03-07 23:44:33 +0000

    Add LS solution exercise 3

--- 5:Loops_and_Iterators/3.rb
+++ 5:Loops_and_Iterators/3.rb
@@ -12,3 +12,17 @@ end
 countdown(5)
 countdown(20)
 countdown(-3)
+
+#LS solution
+def count_to_zero(number)
+    if number <= 0
+        puts number
+    else
+        puts number 
+        count_to_zero(number-1)
+    end
+end
+
+count_to_zero(10)
+count_to_zero(20)
+count_to_zero(-3)
