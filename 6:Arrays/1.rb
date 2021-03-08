66185f7bb81fc3230499c3431011a749eec38302
Martha Kalisz <mskalisz3@gmail.com>
2021-03-08 01:29:38 +0000

    Update exercise 1 solution toinclude LS solition

--- 6:Arrays/1.rb
+++ 6:Arrays/1.rb
@@ -5,4 +5,16 @@ Write a program that checks to see if the number appears in the array.
 
 arr = [1, 3, 5, 7, 9, 11]
 number = 3
-puts arr.include?(number)
+
+if arr.include?(number) 
+  puts "#{number} is in the array."
+end
+
+
+#LS additional solution
+
+arr.each do |num|
+  if num == number
+    puts "#{number} is in the array!"
+  end
+end
