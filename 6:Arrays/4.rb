f991b8c7c1664a7779c850a244ebdc68916ce19c
Martha Kalisz <mskalisz3@gmail.com>
2021-03-08 02:15:34 +0000

    Add notes from LS solution for exercise 4

--- 6:Arrays/4.rb
+++ 6:Arrays/4.rb
@@ -3,10 +3,10 @@
 arr = [15, 7, 18, 5, 12, 8, 5, 1]
 
 1. arr.index(5)
-# Returns: 3 (Asking what is the index for "5" within the array)
+# Returns: 3 (Asking what is the index for the first "5" within the array)
 
 2. arr.index[5]
 # Returns: NoMethodError
 
 3. arr[5]
-# Returns: 8 (Asking for the integer at the 5th index)
+# Returns: 8 (Asking for the integer at the 5th index)  
