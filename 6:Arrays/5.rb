81db0dcd273b7e746bbb1265ad32b02308971aff
Martha Kalisz <mskalisz3@gmail.com>
2021-03-08 02:20:11 +0000

    Solve exercise 5

---
+++ 6:Arrays/5.rb
@@ -0,0 +1,6 @@
+# What is the value of a, b, and c in the following program?
+
+string = "Welcome to America!"
+a = string[6] #Value "e"
+b = string[11] #Value 'A'
+c = string[19] #Value nil ... notice, not an error!
