d5bb72b53e0414037c180fbfda47c4b68de5d960
Martha Kalisz <mskalisz3@gmail.com>
2021-03-08 22:59:50 +0000

    Solve exercise 7

---
+++ 6:Arrays/7.rb
@@ -0,0 +1,5 @@
+#Use the each_with_index method to iterate through an array 
+# of your creation that prints each index and value of the array.
+
+pt_schools = ["University of North Carolina", "Franklin Pierce University", "University of Wisconsin"]
+pt_schools.each_with_index { | value, index | puts "#{index}: #{value}"}
