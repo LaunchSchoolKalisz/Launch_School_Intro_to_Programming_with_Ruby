4c17e95717cfc3095d987bbae828778f06d89328
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 22:43:27 +0000

    Add LS solutions

--- 9:Exercises/3.rb
+++ 9:Exercises/3.rb
@@ -4,3 +4,13 @@ arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 odd_numbers = arr.select { |number| number.odd?}
 p odd_numbers.flatten
+
+# LS Solution
+
+#oneline 
+new_array = arr.select { |number| number % 2 !=0 }
+
+#multi-line
+new_array = arr.select do |number|
+  number % 2 != 0
+end
