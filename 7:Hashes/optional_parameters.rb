4fa8d3a95f6c2bce3c69d82e7b3128e60b5223c8
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 00:22:57 +0000

    Practice with hashes

---
+++ 7:Hashes/optional_parameters.rb
@@ -0,0 +1,13 @@
+#Using a hash to accept optional parameters for more flexibility and expressivity
+
+def greeting(name, options = {})
+  if options.empty?
+    puts "Hi, my name is #{name}"
+  else
+    puts "Hi, my name is #{name} and I'm #{options[:age]}" +
+         " years old and I live in #{options[:city]}."
+  end 
+end
+
+greeting("Bob")
+greeting("Bob", {age: 62, city: "New York City"})
