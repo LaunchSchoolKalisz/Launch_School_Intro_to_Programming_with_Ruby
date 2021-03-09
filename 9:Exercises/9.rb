7e4fad106cd2bcfa43cb76352fe4f069f70d45c6
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 23:43:18 +0000

    Add LSsolutions for exercise 9

--- 9:Exercises/9.rb
+++ 9:Exercises/9.rb
@@ -12,6 +12,9 @@ new_pair = {e: 5}
 h.merge!(new_pair)
 p h
 
+#LS Solution
+h[:e] = 5
+
 #3. Remove all key:value pairs whose value is less than 3.5
 
 h.each_value do |value| 
@@ -21,3 +24,15 @@ h.each_value do |value|
 end
 
 puts h
+
+#LS solution
+ #one line version
+ 
+ h.delete_if { |k, v| v < 3.5}
+ 
+ #multi-line version
+ 
+ h.delete_if do |k, v|
+   v < 3.5
+ end
+ 
