89762a7d6a4dffeb6799cb7622abe7af270c68fc
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 21:56:35 +0000

    Add LS solution adn comments

--- 8:More_Stuff/1.rb
+++ 8:More_Stuff/1.rb
@@ -20,3 +20,22 @@ contains_lab?("experiment")
 contains_lab?("Pans Labyrinth".downcase) #careful! needs to match case as well to be printed!
 contains_lab?("elaborate")
 contains_lab?("polar bear")
+
+#LS solution
+
+def check_in(word)
+  if /lab/i =~ word #i option to fix case sensitivity
+    puts word
+  else
+    puts "No match"
+  end
+end
+
+check_in("laboratory")
+check_in("experiment")
+check_in("Pans Labyrinth")
+check_in("elaborate")
+check_in("polar bear")
+
+# Using regex returns the integer which "lab" starts (truthy)
+# or "nil" which is falsey
