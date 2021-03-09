27a242e906b190a28a404ea6734ceb9111dacb63
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 18:15:24 +0000

    Fix: move exercise 5 to exercise 6 and add prompt for exercise 5

--- 7:Hashes/5.rb
+++ 7:Hashes/5.rb
@@ -1,17 +1,5 @@
-# Given the following code...
-
-x = "hi there"
-my_hash = {x: "some value"}
-my_hash2 = {x => "some value"}
-
-#What's the difference between the two hashes that were created?
-# my_hash is the new naming convention, can be used with Ruby 1.9 and up
-# my_hash2 is the older naming convention
-
-=begin
-WRONG!
-my_hash uses x as a symbol
-my_hash 2 is a variable referencing the string "hi there"
+=begin 
+What method could you use to find out if a Hash 
+contains a specific value in it? 
+Write a program to demonstrate this use.
 =end
-p my_hash
-p my_hash2
