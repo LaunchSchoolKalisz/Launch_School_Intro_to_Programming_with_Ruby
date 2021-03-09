013729b64af4fc5bf6cfcff37905f7d08ef7c21f
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 18:06:27 +0000

    Solve exercise 4

---
+++ 7:Hashes/4.rb
@@ -0,0 +1,7 @@
+=begin
+Given the following expression, how would you access the name of the person?
+=end
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+access_name = person.fetch(:name)
+puts access_name
