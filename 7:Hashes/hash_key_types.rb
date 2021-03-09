9601e29375781cd8ed2080ccd820883c9e947601
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 00:46:56 +0000

    More hash practice

---
+++ 7:Hashes/hash_key_types.rb
@@ -0,0 +1,12 @@
+#string as a key
+{"height" => "6 ft"}
+#array as a key
+{["height"] =>"6 ft"}
+#integer as a key
+{1 => "one"}
+#float as a key
+{45.324 => "forty-five point something"}
+#hash as akey
+{{key: "key"} => "hash as a key"}
+
+#Have to use old style if using something other than symbols as keys!
