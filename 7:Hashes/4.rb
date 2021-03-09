76baae1d4ea1dac6f1f7a79ba6e4fed5cf15edee
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 18:07:27 +0000

    Add LS solution

--- 7:Hashes/4.rb
+++ 7:Hashes/4.rb
@@ -5,3 +5,6 @@ Given the following expression, how would you access the name of the person?
 person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
 access_name = person.fetch(:name)
 puts access_name
+
+#LS solution
+person[:name]
