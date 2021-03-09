f6f894f577977aba34f28fa88f4202b8bc391fb1
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 23:05:38 +0000

    add comments from LS soluttion

--- 9:Exercises/4.rb
+++ 9:Exercises/4.rb
@@ -1,9 +1,9 @@
 # Append 11 to the end of the original array. Prepend 0 to the beginning.
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-arr.push(11)
-arr.unshift(0)
+arr.push(11) #distructive!
+arr.unshift(0)  #distructive! #.first and .shift cant take 0...that's how they staartt new arryys
 p arr
 
 # Additional solution for 11
-arr << 11
+arr << 11 #distructive!
