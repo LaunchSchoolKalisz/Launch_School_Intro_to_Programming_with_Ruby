4ea0db8d1771f9c02ba9dbcd427f3a01c243e273
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 23:51:13 +0000

    Solve exercise 10

---
+++ 9:Exercises/10.rb
@@ -0,0 +1,11 @@
+# Can hash values be arrays? 
+#Yes 
+
+hash = { a: [1, 2, 3], b: [10, 20, 30], c: [100, 200, 300]}
+p hash
+
+#Can you have an array of hashes? (give examples)
+# Not sure, lets try it!
+arr = [{a: 1}, {b: 2}, {c: 3}]
+p arr 
+#looks like yes!
