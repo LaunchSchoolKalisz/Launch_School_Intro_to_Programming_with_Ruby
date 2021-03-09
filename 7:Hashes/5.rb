7126d9513c413e22609c06b7e98a262227d03e64
Martha Kalisz <mskalisz3@gmail.com>
2021-03-09 18:18:51 +0000

    Solve exercise 5

--- 7:Hashes/5.rb
+++ 7:Hashes/5.rb
@@ -3,3 +3,12 @@ What method could you use to find out if a Hash
 contains a specific value in it? 
 Write a program to demonstrate this use.
 =end
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+p person.value?('Bob')
+p person.value?('Martha')
+
+# OR
+
+p person.has_value?('Bob')
+p person.has_value?('Martha')
