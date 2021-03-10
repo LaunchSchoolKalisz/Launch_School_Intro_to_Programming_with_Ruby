27b61291664ed89be63c7b9c8389cf3eaaa16f7e
Martha Kalisz <mskalisz3@gmail.com>
2021-03-10 11:55:07 +0000

    Solve exercise 14

---
+++ 9:Exercises/14.rb
@@ -0,0 +1,15 @@
+#Take the following array:
+
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+     
+=begin
+and turn it into a new array that consists of strings 
+containing one word. (ex. ["white snow", etc...] → ["white", "snow", etc...]. 
+Look into using Array's map and flatten methods, as well as String's split method.
+=end
+
+
+split_a = a.map { |word| word.split} # Map returns each element's value, need map to make the changes distructive (vs each)
+p split_a.flatten
