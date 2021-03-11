45bc01b3bc73db17c04f9e7270f1e7fccfbf5943
Martha Kalisz <mskalisz3@gmail.com>
2021-03-11 15:42:24 +0000

    Solve exercise 3

---
+++ Ruby Basics/Doc 2/3.rb
@@ -0,0 +1,6 @@
+#Assume you have the following code:
+
+s = 'abc def ghi,jkl mno pqr,stu vwx yz'
+puts s.split.inspect # ["abc", "def", "ghi,jkl", "mno", "pqr,stu", "vwx", "yz"]
+puts s.split(',').inspect # ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+puts s.split(',', 2).inspect # ["abc def ghi", "jkl mno pqr,stu vwx yz"]
