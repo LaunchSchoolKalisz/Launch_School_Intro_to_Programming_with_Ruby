d233f63f926e7f2f59756b4e1322c2044b0656d4
Martha Kalisz <mskalisz3@gmail.com>
2021-03-11 22:43:24 +0000

    Add comments to exercise 1

--- Ruby Basics/Variable Scope/1.rb
+++ Ruby Basics/Variable Scope/1.rb
@@ -1,12 +1,14 @@
 # What will the following code print and why? Don't run it until you have tried to answer.
 
-a = 7
+a = 7              #initiate variable "a"
 
-def my_value(b)
-  b += 10
+def my_value(b)    #"b" is local to this method
+  b += 10          #reassign b to b +10, locally in the method. Do not alter the value of a
 end
 
-my_value(a)
+my_value(a)   # Invoke my value method & pass "a" as an argument
 puts a
 
 # the code will print 7, as the method is not destructive to "a"
+
+# IMPORTANT: Numbers in ruby are immutable!!!
