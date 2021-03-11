a0e7604f648161c59b9f463762f8c4bf0161be41
Martha Kalisz <mskalisz3@gmail.com>
2021-03-11 22:46:44 +0000

    Solve exerrcise 2

---
+++ Ruby Basics/Variable Scope/2.rb
@@ -0,0 +1,12 @@
+#What will the following code print, and why? Don't run the code until you have tried to answer.
+
+a = 7
+
+def my_value(a)
+  a += 10
+end
+
+my value(a)
+puts a
+
+#The code will print 7. Numbers are immutable. A is reassigned only within the my_value method
