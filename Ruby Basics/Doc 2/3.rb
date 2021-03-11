a5db0fda354657deb3acae4f84d1b8db6f3a2d7b
Martha Kalisz <mskalisz3@gmail.com>
2021-03-11 15:45:09 +0000

    Docs: add comment exercise 3

--- Ruby Basics/Doc 2/3.rb
+++ Ruby Basics/Doc 2/3.rb
@@ -4,3 +4,5 @@ s = 'abc def ghi,jkl mno pqr,stu vwx yz'
 puts s.split.inspect # ["abc", "def", "ghi,jkl", "mno", "pqr,stu", "vwx", "yz"]
 puts s.split(',').inspect # ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
 puts s.split(',', 2).inspect # ["abc def ghi", "jkl mno pqr,stu vwx yz"]
+
+#non distructive!
