315f168664deb458f4c941771b465949b13d72c6
Martha Kalisz <mskalisz3@gmail.com>
2021-03-11 23:00:36 +0000

    Add LS comments to exercise 3 solution

--- Ruby Basics/Variable Scope/3.rb
+++ Ruby Basics/Variable Scope/3.rb
@@ -3,10 +3,10 @@
 a = 7
 
 def my_value(b)
-  a = b
+  a = b #Initializes new local vaariable, inaccessable outside of my_value method
 end
 
-my_value(a + 5)
+my_value(a + 5) # 12
 puts a
 
 # will STILL print 7. Numbers are immutable. a+5 is referencing my_value, and the initial variable a is uneffected by it
