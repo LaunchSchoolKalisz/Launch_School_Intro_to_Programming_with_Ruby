e9bb05cabab6c16b00432aa235a9e1a4907e9f56
Martha Kalisz <mskalisz3@gmail.com>
2021-03-11 22:50:18 +0000

    Add comments to exercise 10

--- Ruby Basics/Variable Scope/2.rb
+++ Ruby Basics/Variable Scope/2.rb
@@ -6,7 +6,16 @@ def my_value(a)
   a += 10
 end
 
-my value(a)
+my_value(a)
 puts a
 
 #The code will print 7. Numbers are immutable. A is reassigned only within the my_value method
+
+=begin
+LS School explination:
+Method definitions are self-contained with respect to local variables. Local variables outside 
+the method definition are not visible inside the method definition; so the top level a is not 
+available inside my_value. Furthermore, local variables inside the method definition are not 
+visible outside the method definition; so the a inside my_value is a local variable with no 
+top-level visibility
+=end
