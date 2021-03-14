7dd1c14f3e7ddf8a43db9b9199c7e8f2fff4b9c2
Martha Kalisz <mskalisz3@gmail.com>
2021-03-14 22:36:47 +0000

    Add LS solution and comments exercise 4

--- Ruby Basics/Loops1/4.rb
+++ Ruby Basics/Loops1/4.rb
@@ -7,3 +7,21 @@ loop do
     break
   end
 end
+
+# LS solution
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  break if answer == 'yes'
+  puts 'Incorrect answer. Please answer "yes".' #Demonstrate that break doesn't have to be at the end of the loop to do its job
+end
+
+=begin 
+Ways to fix requiring lowercase 'yes' as answer:
+1. break if answer == 'yes' || answer == 'Yes'
+
+or, even better:
+
+2. answer = gets.chomp.downcase
+
+# Providing an error message can be useful in providing a better user experience when dealing with inputs
