cdbff4b7d89b68d442e2278e0469bdff749136c7
Martha Kalisz <mskalisz3@gmail.com>
2021-03-14 22:27:25 +0000

    Solve exercise 4

---
+++ Ruby Basics/Loops1/4.rb
@@ -0,0 +1,9 @@
+# Modify the code below so the loop stops iterating when the user inputs 'yes'
+
+loop do 
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  if answer == 'yes'
+    break
+  end
+end
