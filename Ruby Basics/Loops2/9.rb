7c4ec80a21d9eed4233ac6ef39c93f67482ee4d2
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 20:20:01 +0000

    Add LS soluttion, comments exercise 9

--- Ruby Basics/Loops2/9.rb
+++ Ruby Basics/Loops2/9.rb
@@ -13,10 +13,33 @@ loop do
   number_a += rand(2)
   number_b += rand(2)
   
-  next unless number_a == 5 || number_b == 5
+  next unless number_a == 5 || number_b == 5 #next essentially ignores code below it until the condition is met
   puts number_a
   puts number_b
   puts "5 was reached"
   break
   
+end
+
+=begin
+by placing next before #puts and break, we can skip to the next iteration so they 
+aren't processed until we stop skipping. We use an unless condition for next that
+won't evaluate to true unless either number_a or number_b equal 5. This lets us 
+put whatever we want after next and not have to worry about it being processed 
+until next stops skipping.
+=end
+
+#If not required to use next:
+
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+  
+  if number_a == 5 || number_b == 5
+    puts "5 was reached"
+    break
+  end
 end
