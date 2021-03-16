0da01af9ab6c3d2dbb42525feecceaeada3ce0d6
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 17:01:23 +0000

    Solve exercise 10

---
+++ Ruby Basics/Loops1/10.rb
@@ -0,0 +1,7 @@
+# Your friends just showed up! Given the following array of names, use a for loop to greet each friend individually.
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+#counter = 0 
+
+for name in friends
+  puts "Hello, #{name}!"
+end
