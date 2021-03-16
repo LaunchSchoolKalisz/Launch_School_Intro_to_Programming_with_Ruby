fecb533d8d47252da587cc3127c9cf539c6806eb
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 17:06:23 +0000

    Add LS solition exercise 10

--- Ruby Basics/Loops1/10.rb
+++ Ruby Basics/Loops1/10.rb
@@ -1,7 +1,14 @@
 # Your friends just showed up! Given the following array of names, use a for loop to greet each friend individually.
 friends = ['Sarah', 'John', 'Hannah', 'Dave']
-#counter = 0 
 
 for name in friends
   puts "Hello, #{name}!"
+end
+
+# In a for loop it's typical to use the standard format: for friend in friends, for cat in cats, etc. 
+
+#LS solution
+
+for friend in friends
+  puts "Hello, #{friend}"
 end
