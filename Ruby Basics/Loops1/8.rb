0d0eee5f01532aa7338686d2ecf481ec8997052a
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 16:32:11 +0000

    Solve exercise 8

---
+++ Ruby Basics/Loops1/8.rb
@@ -0,0 +1,15 @@
+# Given the array of several numbers below, use an until loop to print each number.
+
+numbers = [7, 9, 13, 25, 18]
+
+until numbers[18]
+puts numbers
+break
+end
+
+# OR
+
+until numbers == 18
+puts numbers
+break
+end
