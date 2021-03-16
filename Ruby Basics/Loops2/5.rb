d0d608d852e9f83de3f23f90bc661ef6e661a1c5
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 18:48:35 +0000

    Add LS solution exercise 5

--- Ruby Basics/Loops2/5.rb
+++ Ruby Basics/Loops2/5.rb
@@ -11,3 +11,16 @@ loop do
   break if numbers.size == 5
 end
 puts numbers
+
+#LS solution
+
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  
+  numbers.push(input)      #Array#push takes the value of input and add it to the end of the numbers array. Mutates array
+  break if numbers.size == 5
+end
+puts numbers
