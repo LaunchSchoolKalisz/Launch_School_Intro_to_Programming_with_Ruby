aec42a0f427f629d99b338f22021827a93d761be
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 21:23:34 +0000

    Add comments,additional solution exercise 3

--- Ruby Basics/User Input/3.rb
+++ Ruby Basics/User Input/3.rb
@@ -2,8 +2,12 @@
 #then print it if the user enters y. Otherwise, print nothing.
 
 puts ">> Do you want me to print something? (y/n)"
-answer = gets.chomp
+answer = gets.chomp.downcase  # must use chomp on the return value of #gets otherwise the newline character will be included in choice, and choice == 'y' will return false
 if answer == 'y'
   puts "something"
 end
 
+#LS solution
+puts '>> Do you want me to print something? (y/n)'
+choice = gets.chomp
+puts 'something' if choice == 'y'
