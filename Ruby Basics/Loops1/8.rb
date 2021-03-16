f0b75fbd7d1ac5931f85fe36c729a65a3d69db27
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 16:40:59 +0000

    Add LS solution aand comments

--- Ruby Basics/Loops1/8.rb
+++ Ruby Basics/Loops1/8.rb
@@ -13,3 +13,19 @@ until numbers == 18
 puts numbers
 break
 end
+
+# LS Solution
+
+count = 0                    
+
+until count == numbers.size  # uses a counter variable to track the current iteration number
+  puts numbers[count]        # access the array using its index
+  count += 1                 # counter gets incremented upon each iteration
+end
+
+=begin
+Using count helps us in two ways. First, it lets us control the number of iterations
+we want to stop iterating when count equals the length of numbers. 
+Second, we use it to select the next value in the array. 
+This works because count matches the index of each number we want to print
+=end
