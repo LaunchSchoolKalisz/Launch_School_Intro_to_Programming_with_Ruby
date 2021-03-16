be39cf828b3c3907f38b2a07cdf1565720d5db0f
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 18:04:35 +0000

    Add LS solution exercise 2

--- Ruby Basics/Loops2/2.rb
+++ Ruby Basics/Loops2/2.rb
@@ -1,7 +1,29 @@
 # Modify the following code so that the loop stops if number is equal to or between 0 and 10.
 
 loop do
-  number = rand(100)
+  number = rand(100)             #returns a random number between 0 and 99
   puts number
   break if number <= 10 && number >= 0
+end
+
+# LS solution
+
+loop do
+  number = rand(100)
+  puts number
+  
+  if number.between?(0,10)
+    break
+  end
+end
+
+#another solution
+
+loop do
+  number = rand(100)
+  puts number
+  
+  if (1..10).include? number
+    break
+  end
 end
