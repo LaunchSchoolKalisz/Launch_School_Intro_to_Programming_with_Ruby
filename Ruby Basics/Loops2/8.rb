bbf28635d6958773ed3cfacd7e346ed02175fa86
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 19:43:14 +0000

    Add LS solutions and comments exercise 8

--- Ruby Basics/Loops2/8.rb
+++ Ruby Basics/Loops2/8.rb
@@ -6,3 +6,26 @@ until number == 10
   number += 1.next
   puts number
 end
+
+#LS Solution
+number = 0
+
+until number == 10
+  number += 1
+  next if number.odd? #next lets you skip to the next iteration based on certain conditions
+  puts number
+end
+
+#Another solution
+number = 0
+
+until number == 10
+  number += 1
+  next if number % 2 != 0
+  puts number
+end
+=begin
+next needs to be placed after the incremental increase of 1, and before puts on line 6, 
+in to test each iterative number before it is printed so that printing can be skipped in 
+the event of an odd number.
+=end
