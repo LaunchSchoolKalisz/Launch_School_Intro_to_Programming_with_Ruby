bbf223cca43c969e056221c163ee56483d622481
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 21:00:38 +0000

    Add comments, additional solution exercise 2

--- Ruby Basics/User Input/2.rb
+++ Ruby Basics/User Input/2.rb
@@ -4,3 +4,11 @@ puts ">> How old are you in years?"
 age_years = gets.chomp.to_i
 age_months = age_years * 12
 puts "You are #{age_months} months old!"
+
+#What happens if you enter a non-numeric value for the age?
+# Result is 0... the value gets converted to an integer?
+
+#Another solution
+puts '>> What is your age in years?'
+age = gets.chomp.to_i
+puts "You are #{age * 12} months old."
