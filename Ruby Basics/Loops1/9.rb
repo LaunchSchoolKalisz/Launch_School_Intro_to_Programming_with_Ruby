535de23dd3087032f15a219c295f064f8f14d6a6
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 16:51:45 +0000

    Add LS solutioncomments exercise 9

--- Ruby Basics/Loops1/9.rb
+++ Ruby Basics/Loops1/9.rb
@@ -1,7 +1,13 @@
 # The code below shows an example of a for loop. Modify the code so that it only outputs i if i is an odd number.
 
-for i in 1..100
+for i in 1..100 # 2 dot notation is inclusive, 3 dots is exclusive!
   if i.odd?
     puts i
   end
+end
+
+#writing your own logic
+
+for i in 1..100
+  puts i if i % 2 != 0
 end
