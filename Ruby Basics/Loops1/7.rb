88fcc7ab7bb8b71140acb0a1213aafdd5f9f3d74
Martha Kalisz <mskalisz3@gmail.com>
2021-03-16 14:54:44 +0000

    Add LS solution exercise 7

--- Ruby Basics/Loops1/7.rb
+++ Ruby Basics/Loops1/7.rb
@@ -3,6 +3,27 @@
 count = 1
 
 until count == 11
+  puts count      #ouput count prior to incrementing, so if count == 11, evaluates as false,  doesnt print
+  count += 1
+end
+
+#Another option
+count = 0
+
+until count == 10
+  count += 1
+  puts count 
+end
+
+# LS solution
+
+until count > 10
   puts count
   count += 1
 end
+
+=begin
+until is the opposite of the while loop. 
+while iterates until the condition evaluates to false. 
+until iterates until the condition evaluates to true.
+=end
