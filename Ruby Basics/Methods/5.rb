41876fb8c621a244560577749a4c6a227ee82ab4
Martha Kalisz <mskalisz3@gmail.com>
2021-03-17 16:45:12 +0000

    Comments from exercise 5 with LS solution

--- Ruby Basics/Methods/5.rb
+++ Ruby Basics/Methods/5.rb
@@ -3,6 +3,7 @@
 
 def car(make, model)
   puts make + " " + model
+  puts "#{make} #{model}" #another option
 end
 
-car('Toyota', 'Corolla')
+car('Toyota', 'Corolla') #can also print by adding puts here,then it will print and return the string
