32aa520aa947dcb17d209712d22dcced6699ce5b
Martha Kalisz <mskalisz3@gmail.com>
2021-03-17 00:03:44 +0000

    Add comments and LS solution exercise 9

--- Ruby Basics/User Input/9.rb
+++ Ruby Basics/User Input/9.rb
@@ -10,7 +10,7 @@ number_of_lines = nil
 
 loop do
   
-  puts ">> How many output lines do you want? Enter a number >= 3:"
+  puts ">> How many output lines do you want? Enter a number >= 3 (Q to quit):"
   
   number_of_lines = gets.chomp.downcase
   number_of_lines_integer = number_of_lines.to_i
@@ -21,5 +21,32 @@ loop do
     puts ">> That's not enough lines."
   else
     number_of_lines_integer.times {puts "Launch School is the best!"}
+  end
+end
+
+#LS solution
+
+loop do
+  input_string = nil
+  number_of_lines = nil
+  
+  loop do
+    puts '>> How many output lines do you want? ' \
+         'Enter a number >= 3 (Q to Quit):'
+        
+    input_string = gets.chomp.downcase
+    break if input_string == 'q'
+    
+    number_of_lines = input_string.to_i
+    break if number_of_lines >= 3
+    
+    puts ">> That's not enough lines."
+  end
+  
+  break if input_string == 'q' #necessary since a break inside a loop always exits the innermost containing loop, so the break in the inner loop can't exit the outer loop.
+  
+  while number_of_lines > 0
+    puts 'Launch School is the best!'
+    number_of_lines -= 1
   end
 end
