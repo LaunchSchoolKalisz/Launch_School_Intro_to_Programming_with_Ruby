4a3767c17ceb8f5dd3c04bd0ea4b2f7d0f8dfb62
Martha Kalisz <mskalisz3@gmail.com>
2021-03-17 20:59:36 +0000

    Add LS comments exercise 6

--- Ruby Basics/Methods/6.rb
+++ Ruby Basics/Methods/6.rb
@@ -4,13 +4,17 @@ given a boolean as an argument, prints "It's daytime!" if the boolean is true an
 if it's false. Pass daylight into the method as the argument to determine whether it's day or night.
 =end
 
-def time_of_day(daylight)
-  if daylight == true
+def time_of_day(daylight) #daylighht as a parameter
+  if daylight 
     puts "It's daytime"
-  elsif daylight == false
+  else 
     puts "It's nighttime!"
   end
 end
 
-daylight = [true, false].sample
+daylight = [true, false].sample #local variable, not the saame as daylight above(parameter)
 time_of_day(daylight)
+
+#The two daylight variables happen to reference the same value when we invoke time_of_day on line 10, 
+#but the fact that they share a name is irrelevant. The code would function exactly the same had we given 
+#our method parameter a unique name.
