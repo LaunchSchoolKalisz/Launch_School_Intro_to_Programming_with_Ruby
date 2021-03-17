8b8ce798602451dc338baae888cf8ff657d64199
Martha Kalisz <mskalisz3@gmail.com>
2021-03-17 14:34:01 +0000

    Solve exercise 2

---
+++ Ruby Basics/Methods/2.rb
@@ -0,0 +1,8 @@
+def print_me
+  "I'm printing the return value!"
+end
+
+puts print_me #prints the return value of print_me
+
+# In this case, print_me is returning the string "I'm printing the return value!". 
+# Therefore, to print that string, all we need to do is place #puts before the method invocation.
