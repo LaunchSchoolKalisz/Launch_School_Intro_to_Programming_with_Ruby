8aa0f15bfdc8b2c2a6a7aad674f29a15c79ddb08
Martha Kalisz <mskalisz3@gmail.com>
2021-03-17 21:38:53 +0000

    Add LS comments exercise 1

--- Ruby Basics/Return/1.rb
+++ Ruby Basics/Return/1.rb
@@ -6,4 +6,11 @@ end
 
 puts meal
 
-# The code will return a blank screen because we return nil
+=begin
+Every method returns the evaluated result, or return value, of the last line that is executed.
+
+The only way to ensure a specific return value, other than putting it as the last line, is to use return. 
+When return is executed, it immediately exits the method, returning the provided value. The code in this 
+particular exercise would return the string 'Breakfast' with or without the return on line 2 because 
+line 2 is the last (and only) line executed in the meal method.
+=end
