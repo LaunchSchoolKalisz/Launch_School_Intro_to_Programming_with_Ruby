955db727d1d05d298f6192a35c321ce81764af59
Martha Kalisz <mskalisz3@gmail.com>
2021-03-17 23:58:54 +0000

    Solve exercise4

---
+++ Ruby Basics/Return/10.rb
@@ -0,0 +1,11 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def meal
+  'Dinner'
+  puts 'Dinner'
+end
+
+p meal
+
+#Returns nil, as in the last line of the method invokes puts
+# Prints Dinner /n Dinner
