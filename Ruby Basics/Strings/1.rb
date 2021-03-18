fe56726af816331467dceea66bd91edae0bffdda
Martha Kalisz <mskalisz3@gmail.com>
2021-03-18 14:05:56 +0000

    Add LS solution,  comments exercise 1

--- Ruby Basics/Strings/1.rb
+++ Ruby Basics/Strings/1.rb
@@ -1,3 +1,8 @@
 #Create an empty string using the String class and assign it to a variable.
 
 string = ''
+string = ""
+
+#LS solution
+empty_string = String.new #call new on the string class (new is a class method)
+empty_string = ''
