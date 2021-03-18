452108f72a3fcb39aadd8087fc61391f2ec8d3a9
Martha Kalisz <mskalisz3@gmail.com>
2021-03-18 15:00:56 +0000

    Add LS solutions, comments exercise 5

--- Ruby Basics/Strings/5.rb
+++ Ruby Basics/Strings/5.rb
@@ -4,5 +4,19 @@
 first_name = 'John'
 last_name = 'Doe'
 
-full_name = "#{first_name} #{last_name}"
+full_name = "#{first_name} #{last_name}" #string interpolation
 puts full_name
+
+#LS Solution
+full_name = first_name + ' ' + last_name # String#+
+puts full_name
+
+#By using String#+, we must assign the returned string to a variable if we want to use it at 
+#a later time. Otherwise, the returned string will be inaccessible
+#If we didn't want to create a new variable, and instead modify the existing strings, we 
+#could have used String#<<, like this:
+
+first_name << last_name
+puts first_name # => JohnDoe
+
+#String#<< mutates the caller, which means first_name now represents the full name. 
