9f8206fad0b2b3504fef1991e41e1d7bfce88d19
Martha Kalisz <mskalisz3@gmail.com>
2021-03-18 00:58:43 +0000

    Add comments exercise 2

--- Ruby Basics/Conditionals/2.rb
+++ Ruby Basics/Conditionals/2.rb
@@ -2,6 +2,6 @@
 
 sun = ['visible', 'hidden'].sample
 
-unless sun == 'visible'
+unless sun == 'visible'     #unless is opposite of if... only excecutes if condition evaluates false
   puts "The clouds are blocking the sun"
 end
