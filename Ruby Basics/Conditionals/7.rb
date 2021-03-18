017699053ef0fb76974aaa8cdff6d104f195a4f8
Martha Kalisz <mskalisz3@gmail.com>
2021-03-18 12:27:59 +0000

    Add LS comments exercise 7

--- Ruby Basics/Conditionals/7.rb
+++ Ruby Basics/Conditionals/7.rb
@@ -9,3 +9,10 @@ elsif stoplight == 'yellow'
 else
   puts 'Stop!'
 end
+
+=begin
+case statement is best suited for comparing multiple values to the same case. 
+If you look at the if statement, you can see that we compare stoplight to a different value a 
+total of three times. It would be much simpler to reference stoplight once, then list the comparisons. 
+That's where case really shines
+=end
