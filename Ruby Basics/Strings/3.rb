40a5c75a0806560782ceddab7399383c796c06de
Martha Kalisz <mskalisz3@gmail.com>
2021-03-18 14:24:43 +0000

    Add LS comments exercise 3

--- Ruby Basics/Strings/3.rb
+++ Ruby Basics/Strings/3.rb
@@ -13,3 +13,13 @@ p name.downcase == 'DAVE'.downcase
 
 p name.casecmp('RoGeR') == 0
 p name.casecmp('DAVE') == 0
+
+=begin
+#casecmp performs a case-insensitive comparison, meaning it ignores the case of each character. When both compared 
+strings are equal, #casecmp will return 0. That's why, in the solution, we needed the comparison with 0. If the return 
+value equals 0, then we know both strings are equal.
+
+What if both strings aren't equal? If the value of the calling string - name - is less than the provided argument - 
+'RoGeR' - then #casecmp will return -1. Similarly, if the provided argument is less than the calling string, #casecmp 
+will return 1
+=end
