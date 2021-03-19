b77334cbc25e91be2f999ee8c60fc8022ed485ff
Martha Kalisz <mskalisz3@gmail.com>
2021-03-19 00:16:23 +0000

    Solve exercise 9 with LS comments, solutions

--- Ruby Basics/Strings/9.rb
+++ Ruby Basics/Strings/9.rb
@@ -5,3 +5,15 @@ words = 'car human elephant airplane'
 words_ary = words.split(' ')
 words_ary.each { |word| word.insert(-1, 's') }
 puts words_ary
+
+#Another option, but doesn't work as well (insert doesnt appear to be distructive, as puts words on the nextline doesn'twork
+words = 'car human elephant airplane'
+
+puts words.split(' ').each { |word| word.insert(-1, 's') }
+
+#LS solution
+
+words = 'car human elephant airplane'
+words.split(' '). each do |word|
+  puts word + 's'
+end
