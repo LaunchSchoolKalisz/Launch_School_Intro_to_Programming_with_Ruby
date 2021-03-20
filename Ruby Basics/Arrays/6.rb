980c01f1f9ac02da629c5aefc4a5e60a46949849
Martha Kalisz <mskalisz3@gmail.com>
2021-03-20 13:55:47 +0000

    Add comments exercise 6

--- Ruby Basics/Arrays/6.rb
+++ Ruby Basics/Arrays/6.rb
@@ -8,3 +8,9 @@ doubled_numbers = numbers.map do |number|
                   end
 
 p doubled_numbers
+
+# Array#map iterates over an array and returns a new array with each element transformed based on the block's return value
+# This means we can assign the return value of #map to a variable to use at a later time.
+# #map uses the return value of the block as the new element's value. 
+# This means that each element in the new array is the return value of the block at each iteration.
+# Map transforms, each does not
