7533624cead3426bc4cd04a929f857972b401b95
Martha Kalisz <mskalisz3@gmail.com>
2021-03-20 15:57:54 +0000

    Solve exercise 4, add LS solution and comments

---
+++ Ruby Basics/Debugging/4.rb
@@ -0,0 +1,27 @@
+# Magdalena has just adopted a new pet! She wants to add her new dog, Bowser, to the pets hash. 
+# After doing so, she realizes that her dogs Sparky and Fido have been mistakenly removed. 
+# Help Magdalena fix her code so that all three of her dogs' names will be associated with the key :dog in the pets hash.
+
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog][2] = 'bowser'
+
+p pets 
+
+#LS solution
+
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog].push('bowser')
+
+p pets
+
+=begin
+Using element reference, we can access the value associated with the key :dog.
+
+Copy Code
+pets[:dog] #=> ['sparky', 'fido']
+
+Because the returned value is an array, we can use the Array#push method to append an additional element
+(in this case, the String 'bowser') onto the end of the list.
+=end
