a4d9fdfae9234af5b0dfb2d778a3084938ef0c50
Martha Kalisz <mskalisz3@gmail.com>
2021-03-20 14:31:17 +0000

    Add LS comments exercise 1

--- Ruby Basics/Hashes/1.rb
+++ Ruby Basics/Hashes/1.rb
@@ -8,3 +8,19 @@ car = {
   color:   'blue',
   mileage: 80_000
 }
+
+# Remember older syntax:
+{ :number => 1 }
+
+# We use Symbols as keys in most of these exercises, but we can just as easily use Strings. 
+# Symbols are faster and use less memory than Strings, so are preferred as Hash keys. 
+# Note also that Symbols and Strings aren't the same when compared. Consider the following:
+
+:color == 'color' # false (symbol vs string)
+
+# If creating a hash using Strings as keys, keep in mind the required syntax:
+car = {
+  'type'    => 'sedan',
+  'color'   => 'blue',
+  'mileage' => 80_000
+}
