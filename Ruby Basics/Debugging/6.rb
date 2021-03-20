6ff96908fd04e884ead35aa5d46bf4a9672bad72
Martha Kalisz <mskalisz3@gmail.com>
2021-03-20 16:30:53 +0000

    Solve exercise 6

---
+++ Ruby Basics/Debugging/6.rb
@@ -0,0 +1,31 @@
+# You want to have a small script delivering motivational quotes, but with the following code you run into a very common error
+# message: no implicit conversion of nil into String (TypeError). What is the problem and how can you fix it?
+
+#The error is telling us that the method is returning nil, so we aren't returning thhe string that we want to
+
+#one solution: explicit return 
+def get_quote(person)
+  if person == 'Yoda'
+    return 'Do. Or do not. There is no try.'
+  end
+
+  if person == 'Confucius'
+    return 'I hear and I forget. I see and I remember. I do and I understand.'
+  end
+
+  if person == 'Einstein'
+    return 'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+  end
+end
+
+#Another solution, refractor if statements
+def get_quote(person)
+  if person == 'Yoda'
+    'Do. Or do not. There is no try.'
+  elsif person == 'Confucius'
+    'I hear and I forget. I see and I remember. I do and I understand.'
+  elsif person == 'Einstein'
+    'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+  end
+end
+
