8a3584f3df6322c9948e3837dbf9dc6737f12f6a
Martha Kalisz <mskalisz3@gmail.com>
2021-03-20 16:32:49 +0000

    Add LS comments exercise 6

--- Ruby Basics/Debugging/6.rb
+++ Ruby Basics/Debugging/6.rb
@@ -29,3 +29,10 @@ def get_quote(person)
   end
 end
 
+=begin
+Since our original code did not have an explicit return statement, the return value is the evaluated result of the last line executed. 
+The last line is an if statement, which returns the evaluated result of the branch whose condition evaluates to true, or nil if there 
+is no such branch. When we passed 'Confucius' or 'Yoda' in as the argument, the condition person == 'Einstein' evaluated to false, so 
+the last if statement evaluated to nil. This caused our original method to return nil every time an argument other than 'Einstein' is 
+passed in.
+=end
