339a39ff1c091a9141251347ba9a9d5f158bffc6
Martha Kalisz <mskalisz3@gmail.com>
2021-03-20 14:19:49 +0000

    Add LS comments exercise 10

--- Ruby Basics/Arrays/10.rb
+++ Ruby Basics/Arrays/10.rb
@@ -4,3 +4,6 @@ array1 = [1, 5, 9]
 array2 = [1, 9, 5]
 
 p array1 == array2
+
+# Note that == treats two Arrays as equal if they contain the same number of elements, and each element in one Array is equal to 
+# the corresponding element in the second Array(order matters)
